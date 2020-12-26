32db51b702fe961c638ef5ea3b565927b282d3ff
Hechanglin <542177932@qq.com>
2020-12-26 23:33:15 +0800

    添加打包配置

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,24 +1,28 @@
-// Import React dependencies.
-import React, { useEffect, useMemo, useState } from 'react';
-// Import the Slate editor factory.
-import { createEditor } from 'slate';
-
-// Import the Slate components and React plugin.
+import React from 'react';
+import { createEditor, Node } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
+import memoizeOne from 'memoize-one';
 
-const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  // Add the initial value when setting up our state.
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ]);
+export default class Editor extends React.PureComponent {
+  state: { value: Node[] } = {
+    value: [
+      {
+        type: 'paragraph',
+        children: [{ text: 'A line of text in a paragraph.' }],
+      },
+    ],
+  };
 
-  return (
-    <Slate editor={editor} value={value} onChange={(newValue) => setValue(newValue)}>
-      <Editable />
-    </Slate>
-  );
-};
+  render(): React.ReactNode {
+    const editor = this.createEditor();
+    const { value } = this.state;
+
+    return (
+      <Slate editor={editor} value={value} onChange={this.handleChange}>
+        <Editable />
+      </Slate>
+    );
+  }
+  createEditor = memoizeOne(() => withReact(createEditor()));
+  handleChange = (newValue: Node[]): void => this.setState(newValue);
+}
